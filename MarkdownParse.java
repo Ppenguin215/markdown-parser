9265614964c04c515796adb6e7e13a027a100a3f
Ppenguin215 <103155827+Ppenguin215@users.noreply.github.com>
2022-04-15 17:06:27 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,7 +17,11 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
+            
+
             currentIndex = closeParen + 1;
+            
+            System.out.println(currentIndex);
         }
 
         return toReturn;
